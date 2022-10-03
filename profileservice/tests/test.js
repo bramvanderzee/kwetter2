5a41372b21bf32a5a38ebee91e41d3c8ef1af93a
bramvanderzee <bram.vdzee@gmail.com>
2022-10-03 13:20:16 +0200

    added test

--- profileservice/tests/test.js
+++ profileservice/tests/test.js
@@ -1,3 +1,9 @@
 const server = require('../index.js');
 const supertest = require('supertest');
 const requestWithSupertest = supertest(server);
+
+describe('Profile endpoint', () => {
+    it('GET /profile should ...', async () => {
+            expect(res.status).toEqual(200);
+    });
+});
