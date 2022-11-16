862367c95d2cd400f4a13d83b86d001eef53c0d7
bramvanderzee <bram.vdzee@gmail.com>
2022-11-16 12:41:34 +0100

    test

--- profileservice/index.js
+++ profileservice/index.js
@@ -4,8 +4,10 @@ const createServer = require('./server')
 const routes = require('./routes')
 const port = 3000
 const MONGO_URL = process.env.MONGO_URL
-export const MONGO_USER = process.env.MONGO_USER
+const MONGO_USER = process.env.MONGO_USER
 const MONGO_PASS = process.env.MONGO_PASS
+
+export default MONGO_USER
 
 mongoose.connect(MONGO_URL,
     {
