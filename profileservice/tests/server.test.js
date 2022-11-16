1164d8c16df386d1866a2121fa4294093738e2f3
bramvanderzee <bram.vdzee@gmail.com>
2022-11-16 12:02:05 +0100

    removed test

--- profileservice/tests/server.test.js
+++ profileservice/tests/server.test.js
@@ -6,24 +6,24 @@ const mongo_url = process.env.MONGO_URL
 const mongo_user = process.env.MONGO_USER
 const mongo_pass = process.env.MONGO_PASS
 
-#beforeEach((done) => {
-#    mongoose.connect(
-#        mongo_url,
-#        { 
-#            authSource: "admin",
-#            user: mongo_user,
-#            pass: mongo_pass,
-#            useNewUrlParser: true 
-#        },
-#        () => done()
-#    )
-#})
-
-#afterEach((done) => {
-#    mongoose.connection.db.dropDatabase(() => {
-#        mongoose.connection.close(() => done())
-#    })
-#})
+//beforeEach((done) => {
+//    mongoose.connect(
+//        mongo_url,
+//        { 
+//            authSource: "admin",
+//            user: mongo_user,
+//            pass: mongo_pass,
+//            useNewUrlParser: true 
+//        },
+//        () => done()
+//    )
+//})
+//
+//afterEach((done) => {
+//    mongoose.connection.db.dropDatabase(() => {
+//        mongoose.connection.close(() => done())
+//    })
+//})
 
 const app = createServer()
 
