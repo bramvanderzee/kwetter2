8d430d5a7e16faec105f80b5b3c64585be70c896
bramvanderzee <bram.vdzee@gmail.com>
2022-11-16 12:34:17 +0100

    test

--- profileservice/tests/index.test.js
+++ profileservice/tests/index.test.js
@@ -2,5 +2,6 @@ const index = require('../index.js')
 const supertest = require('supertest')
 
 test('has mongo username', () => {
+    console.log(index.MONGO_USER)
     expect(index.MONGO_USER).toBe('bramvanderzee')
 })
