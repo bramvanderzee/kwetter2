a21d9421a90552409bd45034f9fd06033f34dfb9
bramvanderzee <bram.vdzee@gmail.com>
2022-11-17 10:27:18 +0100

    added auth

--- gateway/index.js
+++ gateway/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { setupLogging } = require('./logging')
 const { setupProxies } = require('./proxy')
+const { setupAuth } = require('./auth')
 
 const { ROUTES } = require('./routes')
 
@@ -8,6 +9,7 @@ const app = express()
 const port = 3000
 
 setupLogging(app)
+setupAuth(app, ROUTES)
 setupProxies(app, ROUTES)
 
 app.listen(port, () => {
--- gateway/routes.js
+++ gateway/routes.js
@@ -2,28 +2,24 @@ const ROUTES = [
     {
         url: '/free',
         auth: false,
-        creditCheck: false
-        rateLimit: {
-            windowMs: 15 * 60 * 1000,
-            max: 5
-        },
         proxy: {
             target: 'http://www.google.com',
             changeOrigin: true,
             pathRewrite: {
-                [`^/free`]: '',
+                [`^/free`]: ''
+            }
         }
     },
     {
         url: '/premium',
         auth: true,
-        creditCheck: true,
         proxy: {
             target: 'http://www.google.com',
             changeOrigin: true,
             pathRewrite: {
                 [`^/premium`]: ''
             }
+        }
     }
 ]
 
