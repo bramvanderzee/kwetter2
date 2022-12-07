fa96b7debc90655a21030e02b85a44b83c8ce52e
bramvanderzee <bram.vdzee@gmail.com>
2022-12-07 12:31:12 +0100

    k8s gateway to dockerhub

--- gateway/push.sh
+++ gateway/push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t localhost:5001/gatewayservice:v0.1 .
-docker push localhost:5001/gatewayservice:v0.1
+docker build -t bramvanderzee/kwetter-gateway .
+docker push bramvanderzee/kwetter-gateway
