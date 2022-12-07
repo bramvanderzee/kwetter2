2a366ad0595edfdbf18b37db3cc8d4b6bd62e578
bramvanderzee <bram.vdzee@gmail.com>
2022-12-07 12:42:15 +0100

    moved to dockerhub

--- profileservice/push.sh
+++ profileservice/push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t localhost:5001/profileservice:v0.2 .
-docker push localhost:5001/profileservice:v0.2
+docker build -t bramvanderzee/kwetter-profileservice .
+docker push bramvanderzee/kwetter-profileservice
