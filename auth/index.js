99ce5337eede8f9fb2f90604564e1fb9b83b1393
bramvanderzee <bram.vdzee@gmail.com>
2022-12-15 12:51:17 +0100

    basic auth setup?

---
+++ auth/index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const findOrCreate = require('mongoose-findorcreate')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const app = express()
+
+const PORT = process.env.PORT
+const MONGO_URL = process.env.MONGO_URL
+const MONGO_USER = process.env.MONGO_USER
+const MONGO_PASS = process.env.MONGO_PASS
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+
+const connstring = MONGO_URL.replace('<username>', MONGO_USER).replace('<password>', MONGO_PASS)
+
+mongoose.connect(connstring)
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String
+})
+
+const User = new mongoose.model('User', userSchema)
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackUrl: 'http://localhost:3000/auth/google/home'
+},
+    (accessToken, refreshToken, profile, cb) => {
+        User.findOrCreate({ googleId: profile.id}, (err, user) => {
+            return cb(err, user)
+        })
+    }
+))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/pages/index.html')
+})
+
+app.listen(PORT || 3000)
