18c3f80949e836b53230c4d9924872531bc89acd
bramvanderzee <bram.vdzee@gmail.com>
2023-01-06 12:41:07 +0100

    cors

--- gateway/cors.js
+++ gateway/cors.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const setupCors = (app, routes) => {
     routes.forEach(r => {
         app.use(r.url, cors(), function (req, res, next) {
+            res.set('Access-Control-Allow-Origin', '*');
             next()
         })
     })
